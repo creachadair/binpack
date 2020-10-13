f69f3b2dfbb1dcf559b007235fb2c5f498828fac
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-10-13 01:20:41 -0700

    Add implementation of package binpack.

---
+++ binpack.go
@@ -0,0 +1,213 @@
+// Package binpack implements a compact binary encoding format.
+//
+// A binpack message is a concatenated sequence of tag-value records. A tag is
+// an unsigned integer value, a value is an array of bytes. The tags and values
+// are opaque to the encoding; the caller must provide additional structure as
+// needed.  For example, the application may encode type information in some
+// low-order bits of the tag.
+//
+// Tags are encoded as 1, 2, or 4 bytes, having values up to 2^30-1.  Values
+// are length-prefixed byte arrays up to 2^29-1 bytes in length.
+//
+// The enoding of a tag is as follows:
+//
+//   Byte 0 (index)
+//   +---------------+
+//   |0|   7 bits    | + 0 bytes  : values 0..127 (7 bits)
+//   +---------------+
+//   |1|0| 6 bits    | + 1 byte   : values 0..16383 (14 bits)
+//   +---------------+
+//   |1|1| 6 bits    | + 3 bytes  : values 0..1073741823 (30 bits)
+//   +---------------+
+//
+// The first byte of the tag is called the index, and its high-order two bits
+// determine the size of the tag in bytes (0_=1, 01=2, 11=4).
+//
+// The encoding of a value is as follows:
+//
+//   Byte 0 (index)
+//   +---------------+p
+//   |0|   7 bits    | + 0 bytes         : length 1, value 0..127
+//   +---------------+
+//   |1|0| 6 bits    | + 0 bytes + data  : length 0..63
+//   +---------------+
+//   |1|1|0| 5 bits  | + 1 bytes + data  : length 0..8191
+//   +---------------+
+//   |1|1|1| 5 bits  | + 3 bytes + data  : length 0..536870911
+//   +---------------+
+//
+// The first byte of the value is called the index, and its high-order three
+// bits determine the size of the length prefix. Small single-byte values are
+// encoded directly with a prefix of 0; otherwise the length is 1, 3, or 5
+// bytes.
+//
+package binpack
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+// An Encoder encodes tag-value records to an io.Writer.
+// Call the Encode method to add values. You must call Flush when finished to
+// ensure all buffered output is written to the underlying writer.
+type Encoder struct {
+	buf *bufio.Writer
+}
+
+// NewEncoder constructs an Encoder that writes data to w.
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{buf: bufio.NewWriter(w)}
+}
+
+// Encode appends a single tag-value pair to the output.
+func (e *Encoder) Encode(tag int, value []byte) error {
+	err := writeTag(e.buf, tag)
+	if err == nil {
+		err = writeValue(e.buf, value)
+	}
+	return err
+}
+
+// Flush flushes buffered data to the underlying writer.
+func (e *Encoder) Flush() error { return e.buf.Flush() }
+
+// writeTag appends the encoding of tag to w.
+func writeTag(w *bufio.Writer, tag int) (err error) {
+	if tag < 128 {
+		return w.WriteByte(byte(tag))
+	} else if tag < (1 << 14) {
+		_, err = w.Write([]byte{0x80 | byte(tag>>8), byte(tag & 0xff)})
+	} else if tag < (1 << 30) {
+		_, err = w.Write([]byte{
+			0xC0 | byte(tag>>24), byte(tag >> 16), byte(tag >> 8), byte(tag),
+		})
+	} else {
+		return fmt.Errorf("tag too big (%d > %d)", tag, 1<<30-1)
+	}
+	return
+}
+
+// writeValue writes the encoding of value to w.
+func writeValue(w *bufio.Writer, value []byte) error {
+	n := len(value)
+	if n == 1 && value[0] < 128 {
+		return w.WriteByte(value[0])
+	}
+	var err error
+	if n < (1 << 6) {
+		err = w.WriteByte(0x80 | byte(n))
+		if err == nil {
+			_, err = w.Write(value)
+		}
+	} else if n < (1 << 13) {
+		_, err = w.Write([]byte{0xC0 | byte(n>>8), byte(n)})
+		if err == nil {
+			_, err = w.Write(value)
+		}
+	} else if n < (1 << 29) {
+		_, err = w.Write([]byte{0xE0 | byte(n>>24), byte(n >> 16), byte(n >> 8), byte(n)})
+		if err == nil {
+			_, err = w.Write(value)
+		}
+	} else {
+		return fmt.Errorf("value too big (%d bytes > %d)", len(value), 1<<29-1)
+	}
+	return err
+}
+
+// A Decoder decodes tag-value pairs from an io.Reader.
+type Decoder struct {
+	buf *bufio.Reader
+}
+
+// NewDecoder constructs a Decoder that reads records from r.
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{buf: bufio.NewReader(r)}
+}
+
+// Decode returns the next tag-value record from the reader.
+// At the end of the input, it returns io.EOF.
+func (d *Decoder) Decode() (int, []byte, error) {
+	tag, err := d.readTag()
+	if err != nil {
+		return 0, nil, err
+	}
+	value, err := d.readValue()
+	if err != nil {
+		return tag, nil, err
+	}
+	return tag, value, err
+}
+
+// readTag reads a tag from the current position of the decoder.
+func (d *Decoder) readTag() (int, error) {
+	b, err := d.buf.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	switch v := b >> 6; v {
+	case 0, 1:
+		return int(b), nil
+	case 2:
+		c, err := d.buf.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		return int(b&0x3f)<<8 | int(c), nil
+	default:
+		z, err := d.readInt(3)
+		if err != nil {
+			return 0, err
+		}
+		return int(b&0x3f)<<24 | z, nil
+	}
+}
+
+// readValue reads a value from the current position of the decoder.
+func (d *Decoder) readValue() ([]byte, error) {
+	b, err := d.buf.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	var n int
+	switch v := b >> 5; v {
+	case 0, 1, 2, 3: // literal single-byte value
+		return []byte{b}, nil
+	case 4, 5: // length in index byte
+		n = int(b & 0x3f)
+	case 6: // index + 2
+		c, err := d.buf.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		n = int(b&0x1f)<<8 | int(c)
+	case 7: // 4 bytes after index
+		n, err = d.readInt(3)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Now n is the number of data bytes we need to read.
+	data := make([]byte, n)
+	if _, err := io.ReadFull(d.buf, data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// readInt reads the requested number of bytes from the input, and decodes its
+// value as an unsigned integer in big-endian order.
+func (d *Decoder) readInt(nBytes int) (int, error) {
+	buf := make([]byte, nBytes)
+	if _, err := io.ReadFull(d.buf, buf); err != nil {
+		return 0, err
+	}
+	var z int
+	for _, b := range buf {
+		z = (z << 8) | int(b)
+	}
+	return z, nil
+}
---
+++ binpack_test.go
@@ -0,0 +1,129 @@
+package binpack_test
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/creachadair/binpack"
+)
+
+func TestDecodeEmpty(t *testing.T) {
+	d := binpack.NewDecoder(strings.NewReader(""))
+	tag, value, err := d.Decode()
+	if err != io.EOF {
+		t.Errorf("Decode: got tag=%d, value=%q, err=%v; want EOF", tag, value, err)
+	}
+}
+
+func TestEncodeSeveral(t *testing.T) {
+	input := []string{"cogwheel", "kiss", "failure", "x"}
+
+	var buf bytes.Buffer
+	e := binpack.NewEncoder(&buf)
+
+	// Encode the inputs using their lengths as tags.
+	for _, s := range input {
+		if err := e.Encode(len(s), []byte(s)); err != nil {
+			t.Fatalf("Encode %q failed: %v", s, err)
+		}
+	}
+	if err := e.Flush(); err != nil {
+		t.Fatalf("Flush failed: %v", err)
+	}
+
+	// Verify the binary format is right (hand-constructed).
+	const want = "\x08\x88cogwheel\x04\x84kiss\x07\x87failure\x01x"
+	if got := buf.String(); got != want {
+		t.Errorf("Encoded result: got %q, want %q", got, want)
+	}
+
+	// Verify we can get the original values back out, in order.
+	d := binpack.NewDecoder(&buf)
+	for i := 0; ; i++ {
+		tag, value, err := d.Decode()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			t.Fatalf("Decode: unexpected error: %v", err)
+		}
+		if tag != len(value) {
+			t.Errorf("Decode: tag=%d, want %d", tag, len(value))
+		}
+		if got := string(value); got != input[i] {
+			t.Errorf("Decode: value=%q, want %q", got, input[i])
+		}
+	}
+}
+
+func TestRoundTrip(t *testing.T) {
+	tests := []struct {
+		tag   int
+		value string
+		want  string
+	}{
+		// Single-byte values.
+		{0, "\x00", "\x00\x00"},                 // value fits in 7 bits
+		{0, "\x10", "\x00\x10"},                 // "
+		{1, "\x8f", "\x01\x81\x8f"},             // value requires > 7 bits
+		{129, "\x00", "\x80\x81\x00"},           // tag requires > 7 bits
+		{18000, "\x7f", "\xC0\x00\x46\x50\x7f"}, // tag requires > 14 bits
+
+		// Empty value.
+		{0, "", "\x00\x80"},
+
+		// Non-empty values.
+		{0, "foo", "\x00\x83foo"},
+		{15, "crazytrain", "\x0f\x8acrazytrain"},
+		{72, "this string is seventy-five bytes in length and that may surprise you maybe",
+			"\x48\xc0\x4bthis string is seventy-five bytes in length and that may surprise you maybe",
+		}, // value exceeds 1-byte length marker
+		{170, strings.Repeat("a", 9000),
+			"\x80\xaa\xE0\x00\x23\x28" + strings.Repeat("a", 9000),
+		}, // value exceeds 2-byte length marker
+	}
+
+	for _, test := range tests {
+		var buf bytes.Buffer
+		e := binpack.NewEncoder(&buf)
+		if err := e.Encode(test.tag, []byte(test.value)); err != nil {
+			t.Errorf("Encode(%d, %q): unexpected error: %v", test.tag, capLen(test.value), err)
+			continue
+		} else if err := e.Flush(); err != nil {
+			t.Errorf("Flush: unexpected error: %v", err)
+			continue
+		}
+		got := buf.String()
+		if got != test.want {
+			t.Errorf("Encode(%d, %q): got %q, want %q",
+				test.tag, capLen(test.value), capLen(got), capLen(test.want))
+		}
+
+		// Ensure we can round-trip the value.
+		d := binpack.NewDecoder(&buf)
+		tag, value, err := d.Decode()
+		if err != nil {
+			t.Errorf("Decode failed: %v", err)
+		}
+		if tag != test.tag {
+			t.Errorf("Decode tag: got %d, want %d", tag, test.tag)
+		}
+		if got := string(value); got != test.value {
+			t.Errorf("Decode value: got %q, want %q", capLen(got), capLen(test.value))
+		}
+
+		// No extra garbage allowed.
+		if tag, value, err := d.Decode(); err != io.EOF {
+			t.Errorf("Decode: got %v, %q, %v; want EOF", tag, capLen(string(value)), err)
+		}
+	}
+}
+
+func capLen(s string) string {
+	const maxLen = 30
+	if len(s) > maxLen {
+		return s[:maxLen-3] + "..."
+	}
+	return s
+}
